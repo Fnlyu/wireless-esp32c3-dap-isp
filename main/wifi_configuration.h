d32e16c0ce06a3693d0cba63b856b33828b0d432
FBIx0 <FBIx0024@gmail.com>
2025-03-28 16:00:07 +0800

    初始化项目

---
+++ main/wifi_configuration.h
@@ -0,0 +1,70 @@
+/**
+ * @file wifi_configuration.h
+ * @brief Fill in your wifi configuration information here.
+ * @version 0.1
+ * @date 2020-01-22
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+#ifndef __WIFI_CONFIGURATION__
+#define __WIFI_CONFIGURATION__
+
+
+static struct {
+    const char *ssid;
+    const char *password;
+} wifi_list[] __attribute__((unused)) = {
+    // {.ssid = "OTA", .password = "12345678"},
+    // {.ssid = "DAP", .password = "12345678"},
+    {.ssid = "313", .password = "313313313"},
+    // Add your WAP like this:
+    // {.ssid = "your ssid", .password = "your password"},
+};
+
+#define WIFI_LIST_SIZE (sizeof(wifi_list) / sizeof(wifi_list[0])) // wifi_list的大小
+
+#define USE_MDNS       1  //打开mDNS功能
+// Use the address "dap.local" to access the device
+#define MDNS_HOSTNAME "dap"
+#define MDNS_INSTANCE "DAP mDNS"
+//
+
+#define USE_STATIC_IP 0         //如果你想使用静态IP地址，请设置为1
+// If you don't want to specify the ip configuration, then ignore the following items.
+#define DAP_IP_ADDRESS 192, 168, 137, 123
+#define DAP_IP_GATEWAY 192, 168, 137, 1
+#define DAP_IP_NETMASK 255, 255, 255, 0
+//
+
+#define USE_OTA              1              //打开OTA功能  
+
+#define USE_UART_BRIDGE      1              //打开串口桥接功能
+#define UART_BRIDGE_PORT     1234           //串口桥接TCP端口号
+#define UART_BRIDGE_BAUDRATE 115200         //串口波特率
+//
+
+// DO NOT CHANGE
+#define USE_TCP_NETCONN 0               // 使用TCP netconn API，如果你想使用TCP netconn API，请设置为1
+
+#define PORT                3240        // USBIP TCP端口号，存疑？？？？
+#define CONFIG_EXAMPLE_IPV4 1           
+#define USE_KCP             0             // 使用KCP协议，如果你想使用KCP协议，请设置为1
+#define MTU_SIZE            1500            // KCP MTU大小
+//
+
+#if (USE_TCP_NETCONN == 1 && USE_KCP == 1)
+#error Can not use KCP and TCP at the same time!
+#endif
+
+#if (USE_KCP == 1)
+#warning KCP is a very experimental feature, and it should not be used under any circumstances. Please make sure what you are doing. Related usbip version: https://github.com/windowsair/usbip-win
+#endif
+
+
+extern int printf(const char *, ...);
+inline int os_printf(const char *__restrict __fmt, ...)  {
+    return printf(__fmt, __builtin_va_arg_pack());
+}
+
+#endif
